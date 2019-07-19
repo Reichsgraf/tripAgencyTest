1dcf446cc5ce004b387b22c782c4748184169b46
Reichsgraf <graf.von.gers@gmail.com>
2019-07-19 16:42:37 +0300

    little change in the name of commit

---
+++ src/app/core/app-footer/app-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  styleUrls: ['app-footer.component.scss'],
+  templateUrl: 'app-footer.component.html'
+})
+export class AppFooterComponent {
+  constructor() {}
+
+  nothingToDo() {}
+}
