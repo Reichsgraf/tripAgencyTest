a9401ca5c31719e22706f35809b747ba9d102f67
Reichsgraf <graf.von.gers@gmail.com>
2019-07-25 13:08:50 +0300

    little cosmetic improvements of carousel

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -9,6 +9,11 @@ import { Trip } from '../../shared/models/trip.interface';
   templateUrl: 'home.component.html'
 })
 export class HomeComponent implements OnInit {
+  slides = [
+    { text: '1', imageUrl: 'assets/sliders/slider2.jpg'},
+    { text: '2', imageUrl: 'assets/sliders/slider3.jpg'},
+    { text: '3', imageUrl: 'assets/sliders/slider4.jpg'},
+  ];
   trips: Array<Trip>;
 
   constructor(
